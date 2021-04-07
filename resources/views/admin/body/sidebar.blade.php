ea683fca81d6c9d6fdd20be92dd25ed9d3183fcd
azaharul-just <47347012+azaharul-just@users.noreply.github.com>
2021-04-07 10:20:42 +0600

    Employee Monthly Salary

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -112,6 +112,7 @@
             <li class="{{($route == 'employee.salary.view')?'active':''}}"><a href="{{route('employee.salary.view')}}"><i class="ti-more"></i>Employee Salary</a></li> 
             <li class="{{($route == 'employee.leave.view')?'active':''}}"><a href="{{route('employee.leave.view')}}"><i class="ti-more"></i>Employee Leave</a></li> 
             <li class="{{($route == 'employee.attendance.view')?'active':''}}"><a href="{{route('employee.attendance.view')}}"><i class="ti-more"></i>Employee Attendance</a></li> 
+            <li class="{{($route == 'employee.monthly.salary')?'active':''}}"><a href="{{route('employee.monthly.salary')}}"><i class="ti-more"></i>Employee Monthly Salary</a></li> 
             
           </ul>
         </li> 
